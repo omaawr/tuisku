0bcedd339e4ef942181909ffb182bc521664a9f4
Omar <omawr@tuta.io>
2026-08-01 01:04:18 +0300

    feat: generate encryption keys in a more secure method

--- app/src/main/kotlin/com/omaawr/tuisku/components/EncryptionManager.kt
+++ app/src/main/kotlin/com/omaawr/tuisku/components/EncryptionManager.kt
@@ -3,16 +3,19 @@ package com.omaawr.tuisku.components
 import com.omaawr.tuisku.settings.Preferences
 import kotlinx.coroutines.flow.first
 import java.io.File
+import java.security.SecureRandom
 import javax.crypto.Cipher
 import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
+import kotlin.random.asKotlinRandom
 
 class EncryptionManager(
     private val prefs: Preferences
 ) {
     suspend fun encryptFile(bytes: ByteArray, filePath: String) {
-        val key = prefs.getEncryptionKey().first().toByteArray()
-        val iv = prefs.getIVKey().first().toByteArray()
+        val key = Base64.decode(prefs.getEncryptionKey().first())
+        val iv = Base64.decode(prefs.getIVKey().first())
 
         val cipher = Cipher.getInstance("ChaCha20")
         val mode = Cipher.ENCRYPT_MODE
@@ -24,8 +27,8 @@ class EncryptionManager(
     }
 
     suspend fun decryptFile(bytes: ByteArray): String {
-        val key = prefs.getEncryptionKey().first().toByteArray()
-        val iv = prefs.getIVKey().first().toByteArray()
+        val key = Base64.decode(prefs.getEncryptionKey().first())
+        val iv = Base64.decode(prefs.getIVKey().first())
 
         val cipher = Cipher.getInstance("ChaCha20")
         val mode = Cipher.DECRYPT_MODE
@@ -37,9 +40,11 @@ class EncryptionManager(
     }
 
     fun generateKey(length: Int): String {
-        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
-        return (1..length)
-            .map { allowedChars.random() }
-            .joinToString("")
+        val secureRandom = SecureRandom()
+        val byteArray = ByteArray(length)
+
+        secureRandom.asKotlinRandom().nextBytes(byteArray)
+
+        return Base64.encode(byteArray)
     }
 }
