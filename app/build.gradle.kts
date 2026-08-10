1d3b58b8a39e64028ffec10fd17e3379c4fd5fd9
Omar <omawr@tuta.io>
2026-08-10 09:45:38 +0300

    chore: bump version to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.omaawr.tuisku"
         minSdk = 28
         targetSdk = 37
-        versionCode = 11
-        versionName = "1.1.1-1"
+        versionCode = 12
+        versionName = "1.1.2"
     }
 
     dependenciesInfo {
