6b3761231433370698565b9d57a12d5d2c130d69
Omar <omawr@tuta.io>
2026-08-15 22:22:03 +0300

    chore: bump version to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.omaawr.tuisku"
         minSdk = 28
         targetSdk = 37
-        versionCode = 13
-        versionName = "1.1.2-1"
+        versionCode = 14
+        versionName = "1.1.3"
     }
 
     dependenciesInfo {
