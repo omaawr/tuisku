ed4371981806e871205c2e9d3405be8ed7907891
Omar <omawr@tuta.io>
2026-08-18 17:38:29 +0300

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.omaawr.tuisku"
         minSdk = 28
         targetSdk = 37
-        versionCode = 16
-        versionName = "1.1.4"
+        versionCode = 17
+        versionName = "1.2.0"
     }
 
     dependenciesInfo {
